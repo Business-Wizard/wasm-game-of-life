63703a614b2d12fcbabd3e634df340b23156ea3a
Business-Data-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2023-06-26 20:09:52 -0600

    feat: implement Universe new & tick

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,93 @@ pub struct Universe {
     height: u32,
     cells: Vec<Cell>,
 }
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count: u8 = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row: u32 = (row + delta_row) % self.height;
+                let neighbor_col: u32 = (column + delta_col) % self.width;
+                let idx: usize = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next_universe: Vec<Cell> = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx: usize = self.get_index(row, col);
+                let cell: Cell = self.cells[idx];
+                let live_neighbors: u8 = self.live_neighbor_count(row, col);
+
+                let next_cell: Cell = match (cell, live_neighbors) {
+                    (Cell::Alive, 0..=1) => Cell::Dead, // underpopulation
+                    (Cell::Alive, 2..=3) => Cell::Alive,
+                    (Cell::Alive, 4..) => Cell::Dead, // overpopulation
+                    (Cell::Dead, 3) => Cell::Alive,   // reproduction
+                    (otherwise, _) => otherwise,
+                };
+                next_universe[idx] = next_cell;
+            }
+        }
+        self.cells = next_universe;
+    }
+
+    pub fn new() -> Universe {
+        let width: u32 = 64;
+        let height: u32 = 64;
+
+        let cells: Vec<Cell> = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
